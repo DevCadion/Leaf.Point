8e2efa41d7cfaa8c6a8830d561e34110acc5b40a
Caio Moreira <caio.santos.m@hotmail.com>
2024-09-04 17:07:30 -0300

    Header01

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,54 +1,14 @@
 import React from "react";
-import { Text, View } from "react-native";
-import AppLoading from "expo-app-loading";
-import * as SplashScreen from 'expo-splash-screen';
-import {useEffect} from 'react';
-
-// Imports fontes.
-
-import {
-  useFonts,
-  Poppins_300Light,
-  Poppins_400Regular,
-  Poppins_500Medium,
-  Poppins_700Bold,
-  Poppins_800ExtraBold,
-}from "@expo-google-fonts/poppins";
-
-//--------------------------------------------//
-
-import{DMSans_400Regular}from "@expo-google-fonts/dm-sans";
-import{DMSerifDisplay_400Regular}from "@expo-google-fonts/dm-serif-display";
+import Page from "../components/Header/MarcaHader"
+import {Text, TextInput, View} from 'react-native';
+//import login from "../components/logins/login";
 
 
-//fim das importações de fontes.
+const MyComponent: React.FC = () => (
+    <>
+        <Page />
+        <Page />
+    </>
+);
 
-SplashScreen.preventAutoHideAsync();
-
-export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    Poppins_300Light,
-    Poppins_400Regular,
-    Poppins_500Medium,
-    Poppins_700Bold,
-    Poppins_800ExtraBold,
-    DMSans_400Regular,
-    DMSerifDisplay_400Regular,
-  });
-  useEffect(() => {
-    if (loaded || error) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded, error]);
-
-  if (!loaded && !error) {
-    return null;
-  }
-
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontFamily: 'Poppins_700Bold', fontSize: 18 }}>
-        Test Leaf </Text>
-    </View>
-  );
-}
+export default MyComponent;

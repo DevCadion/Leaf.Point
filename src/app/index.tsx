2a5e0b5edab252e9469b20bf59766b178896c702
Caio Moreira <caio.santos.m@hotmail.com>
2024-09-04 09:15:45 -0300

    sobetudo

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+      }}
+    >
+      <Text>Teste Leaf</Text>
+    </View>
+  );
+}
+

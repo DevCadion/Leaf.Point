9cd2ac0108c6eea52fd4acb718046b4c53f128cf
Eder Sousa <Ederbruno2017@gmail.com>
2024-09-18 16:18:57 -0300

    Iniciando a primeira pagina

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+
+import { NavigationContainer } from '@react-navigation/native'
+import Routes from '../routes'
+
+export default function App() {
+    <NavigationContainer>
+    <Routes/>
+    </NavigationContainer>
+}
